480fef2d185e0c8b34bc9f738367513b267a1858
araichidu <yanonay3@gmail.com>
2022-07-16 02:03:15 +0900

    Updates

---
+++ precache-manifest.6e04468872d122fe3155a88220fff7d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f698a3beb040d7b110d8",
+    "url": "/reactF/static/css/main.59c45cd7.chunk.css"
+  },
+  {
+    "revision": "f698a3beb040d7b110d8",
+    "url": "/reactF/static/js/main.f698a3be.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/reactF/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/reactF/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "33a1e1c73702107c0476",
+    "url": "/reactF/static/js/runtime~main.33a1e1c7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reactF/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1ead7602b0f2d636cb658f2a551a55d3",
+    "url": "/reactF/index.html"
+  }
+];
